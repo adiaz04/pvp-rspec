46414e12e1d21cca5ce152414cb9da0065252b63
Angel Diaz <angel.diaz@strongmind.com>
2023-06-29 08:26:40 -0700

    add initial spec for testing match

--- main_spec.rb
+++ main_spec.rb
@@ -4,5 +4,22 @@ require 'faker'
 require_relative 'main'
 
 describe 'main' do
+  let(:match) { 'match' }
+  let(:player1) { 'player1' }
+  let(:player2) { 'player2' }
 
+  it 'should allow allow 2 players to register for a match' do
+    # setup
+    attributes = {
+      player1: player1,
+      player2: player2
+    }
+
+    # act
+    match.setup(**attributes)
+
+    # assert
+    expect(match.players).to eq([player1, player2])
+
+  end
 end
