b1c7dcae6d6486a7f4f5d4a8040283fc0a9f38d6
Angel Diaz <angel.diaz@strongmind.com>
2023-06-29 08:38:40 -0700

    added context to current specs

--- main_spec.rb
+++ main_spec.rb
@@ -4,16 +4,18 @@ require 'faker'
 require_relative 'main'
 
 describe 'main' do
-  let(:player1) { 'player1' }
-  let(:player2) { 'player2' }
-  let(:attributes) { { player1: player1, player2: player2 } }
-  let(:match) { Match.new(**attributes) }
+  context 'match hasnt started' do
+    let(:player1) { 'player1' }
+    let(:player2) { 'player2' }
+    let(:attributes) { { player1: player1, player2: player2 } }
+    let(:match) { Match.new(**attributes) }
 
-  it 'should allow allow 2 players to register for a match' do
-    expect(match.players).to eq([player1, player2])
-  end
+    it 'should allow allow 2 players to register for a match' do
+      expect(match.players).to eq([player1, player2])
+    end
 
-  it 'should have an initial state of match_starting_soon' do
-    expect(match.state).to eq('match_starting_soon')
+    it 'should have an initial state of match_starting_soon' do
+      expect(match.state).to eq('match_starting_soon')
+    end
   end
 end
